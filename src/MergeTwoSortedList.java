fccfac1d443918285e9519690c111f2505988bbb
nurselaine <Elaine.t.huynh@gmail.com>
2024-03-13 00:43:37 -0700

    Merge two sorted list fix

--- src/MergeTwoSortedList.java
+++ src/MergeTwoSortedList.java
@@ -10,17 +10,17 @@ public class MergeTwoSortedList {
         }
     }
 
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2){
-        if(l1 == null){
-            return l2;
-        } else if (l2 == null){
-            return l1;
-        } else if (l1.val < l2.val){
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            return list2;
+        } else if (list2 == null){
+            return list1;
+        } else if (list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
     }
 
