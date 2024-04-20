4e4f912e82f49f2c4d03568640e9c6b67538633c
nurselaine <Elaine.t.huynh@gmail.com>
2024-04-19 22:23:28 -0700

    Reverse Words

---
+++ src/leetcode150/ReverseWords.java
@@ -0,0 +1,45 @@
+package leetcode150;
+
+public class ReverseWords {
+
+    public static void main(String[] args){
+        String s = "  hello world  ";
+        String res = reverseWords(s);
+        System.out.println(res);
+
+        s = "the sky is blue";
+        res = reverseWords(s);
+        System.out.println(res);
+    }
+
+
+    /**
+     * given a string, reverse the order of words
+     *
+     * PLAN
+     * trim the string to remove extra spaces on the outside
+     * Create a stringbuilder string
+     * translate string to array
+     * iterate over array from right to left
+     * check if
+     * 1. value is " "
+     * - continue on
+     * otherwise
+     * add current array element to string builder
+     *
+     * return string builder
+     * */
+    public static String reverseWords(String s){
+        s = s.trim();
+        String[] sArr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = sArr.length - 1; i >= 0; i--){
+            if(!sArr[i].equals(" ")){
+                sb.append(sArr[i] + " ");
+            }
+        }
+
+        return sb.toString().trim();
+    }
+}
--- src/leetcode150/lengthOfLastWord.java
+++ src/leetcode150/lengthOfLastWord.java
@@ -3,17 +3,26 @@ package leetcode150;
 public class lengthOfLastWord {
 
     public static void main(String[] args){
-        String s = "Hello World";
-        int res = lengthOfLastWord(s);
-        System.out.println(res);
+//        String s = "Hello World";
+//        int res = lengthOfLastWord(s);
+//        System.out.println(res);
+//
+//        s = "    fly me   to    the moon  ";
+//        res = lengthOfLastWord(s);
+//        System.out.println(res);
+//
+//        s = "Leffy is still joyboy";
+//        res = lengthOfLastWord(s);
+//        System.out.println(res);
 
-        s = "    fly me   to    the moon  ";
-        res = lengthOfLastWord(s);
-        System.out.println(res);
-
-        s = "Leffy is still joyboy";
-        res = lengthOfLastWord(s);
-        System.out.println(res);
+        String[] strs = new String[]{"flower", "flow", "flight"};
+//        String result = longestCommonPrefix(strs);
+        String result = lcpDivideAndConquer(strs);
+        System.out.println(result);
+        strs = new String[]{"dog", "racecar", "car"};
+//        result = longestCommonPrefix(strs);
+        result = lcpDivideAndConquer(strs);
+        System.out.println(result);
     }
 
     /**
@@ -27,6 +36,78 @@ public class lengthOfLastWord {
     public static int lengthOfLastWord(String s){
         String[] strArr = s.split(" ");
         return strArr[strArr.length - 1].length();
+    }
+
+
+    /**
+     * Write a fn to find the longest common prefix amongst
+     * all strings in given array
+     *
+     * PLAN
+     * using first word
+     * iterate over all other words
+     * while:
+     * 1. first word length is > current word
+     * 2. first word != current word
+     * substring first word by 1 char
+     *
+     * return first word
+     * */
+    public static String longestCommonPrefix(String[] strs){
+        String prefix = strs[0];
+
+
+        for(int i = 1; i < strs.length; i++){
+
+            while(!prefix.equals(strs[i])){
+                if(prefix.length() == strs[i].length()){
+                    prefix = prefix.substring(0, prefix.length() - 1);
+                    strs[i] = strs[i].substring(0, strs[i].length() - 1);
+                } else {
+                    int len = Math.min(prefix.length(), strs[i].length());
+                    prefix = prefix.substring(0, len);
+                    strs[i] = strs[i].substring(0, len);
+                }
+            }
+
+            if(prefix.isEmpty()){
+                return prefix;
+            }
+        }
+        return prefix;
+    }
+
+
+    /**
+     * Split strs array into 2 subarrays where leftArr returns a common prefix
+     * and rightArr returns another prefix, then we use those two prefices
+     * to check the common prefix between the 2 subarrays and return it
+     *
+     * PLAN
+     * use a helper function
+     * */
+    public static String lcpDivideAndConquer(String[] strs){
+        return divide(strs, 0, strs.length - 1);
+    }
+
+    private static String divide(String[] strs, int l, int r){
+        if(l >= r){
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String left = divide(strs, l, mid);
+            String right = divide(strs, mid + 1, r);
+            return combine(left, right);
+        }
+    }
+
+    private static String combine(String l, String r){
+        int min = Math.min(l.length(), r.length());
+        int index = 0;
+        while(index < min && l.charAt(index) == r.charAt(index)){
+            index++;
+        }
+        return l.substring(0, index);
     }
 }
 
